4da12a5188f455596a398b37d2528f24c5e2699d
Elvin0802 <elvincode1517@gmail.com>
2024-03-11 22:06:22 +0400

    Add project files.

---
+++ HW-10 for L11/Models/DebtorClass.cs
@@ -0,0 +1,27 @@
+﻿namespace Lesson11.Models;
+
+public class Debtor
+{
+	public Debtor(string fullName, DateTime birthDay, string phoneNum, string email, string address, double debt)
+	{
+		this.FullName = fullName;
+		this.BirthDay = birthDay;
+		this.Phone = phoneNum;
+		this.Email = email;
+		this.Address = address;
+		this.Debt = debt;
+	}
+
+
+	public string FullName { get; set; }
+	public DateTime BirthDay { get; set; }
+	public string Phone { get; set; }
+	public string Email { get; set; }
+	public string Address { get; set; }
+	public double Debt { get; set; }
+
+	public override string ToString()
+	{
+		return $"{this.FullName} | {this.BirthDay.ToShortDateString()} | {this.Phone} | {this.Email} | {this.Address} | Debt : {this.Debt} AZN";
+	}
+}
